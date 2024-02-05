36d28e651279cf68eef7c802401f4da71ead45fc
Sobirjon <152675554+Kirito016002@users.noreply.github.com>
2024-02-05 21:25:10 +0500

    Delete Project directory
    Keraksiz fayl

--- Project/main/urls.py
+++
@@ -1,9 +0,0 @@
-from . import views
-from django.urls import path
-
-app_name = 'main'
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('product/<int:id>', views.product_detail, name='product_detail')
-]
